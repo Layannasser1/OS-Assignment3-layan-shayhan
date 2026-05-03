0c9082e37874aea4b09ef49f98b6945672b27186
Layannasser1 <445052175@std.psau.edu.sa>
2026-05-03 22:15:13 +0300

    Add ReentrantLock to protect execution log

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -38,6 +38,7 @@ class SharedResources {
     
     // TODO #1: Add a ReentrantLock(s) here to protect critical sections
     // Example: public static final ReentrantLock lock = new ReentrantLock();
+      public static final java.util.concurrent.locks.ReentrantLock lock = new java.util.concurrent.locks.ReentrantLock();
     
     // TODO #2: Add a Semaphore to limit concurrent process execution
     // Example: public static final Semaphore cpuSemaphore = new Semaphore(1);
